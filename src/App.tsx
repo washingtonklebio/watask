fcbfeaf55029ec3fb85fc29490b209e00a0832a6
Washington Correia <washingtonkle@gmail.com>
2021-09-20 23:10:07 -0300

    feat: implementation of project routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Modal from 'react-modal'
-import { Dashboard } from "./components/Dashboad"
-import { Header } from "./components/Header"
 
-import { GlobalStyle } from "./styles/global"
-import { TaskModal } from './components/TaskModal'
+
+import { GlobalStyle } from './styles/global';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
 
 Modal.setAppElement('#root');
 
 export function App() {
-  const [isNewTaskModalOpen, setIsNewTaskModalOpen] = useState(false);
-
-  function handleOpenNewTaskModal() {
-      setIsNewTaskModalOpen(true);
-  }
- 
-  function handleCloseNewTaskModal() {
-      setIsNewTaskModalOpen(false);
-  }
 
   return (
-    <>
-      <Header onOpenNewTaskModal={handleOpenNewTaskModal} />
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
 
-      <Dashboard />
-
-      <TaskModal isOpen={isNewTaskModalOpen} onRequestClose={handleCloseNewTaskModal} />
-      
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
